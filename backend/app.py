32ec5eba4df48d9f8b08eb2a48dd338f10c180e9
Rafel-Melo <rafael.vilas@gmail.com>
2025-06-04 17:35:15 -0300

    Definição da rota de detecção das placas

--- backend/app.py
+++ backend/app.py
@@ -21,22 +21,26 @@ def detectar_placa():
     extensao = Path(imagem.filename).suffix
     nome_arquivo = f"{uuid.uuid4()}{extensao}"
     caminho_arquivo = Path('assets').joinpath(nome_arquivo).as_posix()
+    # caminho_arquivo = Path(__file__).parents[0].joinpath('assets', nome_arquivo).as_posix()
 
     imagem.save(caminho_arquivo)
 
+    mimetype, _ = mimetypes.guess_type(caminho_arquivo)
+
     detector = OpenALPRDetector()
     # detector = PlateRecognizerAPI()
-
-    mimetype, _ = mimetypes.guess_type(caminho_arquivo)
     
     if mimetype.startswith('image'):
         resultados = detector.find_plate(caminho_imagem=caminho_arquivo)
-    elif mimetype.startswith('video'):
+    elif mimetype.startswith('video') and hasattr(detector, 'find_plate_in_video'):
         resultados = detector.find_plate_in_video(caminho_arquivo, 30)
     else:
         return jsonify({'error': 'Não é possível fazer a detecção nesse tipo de arquivo'}), 400
 
-    return jsonify({'success': resultados})
+    if resultados:
+        return jsonify({'success': resultados}), 200
+    
+    return jsonify({'error': 'Placa não detectada na imagem'}), 404
 
 if __name__ == '__main__':
     app.run()
