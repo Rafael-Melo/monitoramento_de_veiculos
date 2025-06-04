3c86604c8833d9ce5fe07741848d290175a17e77
Rafel-Melo <rafael.vilas@gmail.com>
2025-06-04 10:01:56 -0300

    Criando classes

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ from backend.controllers.openalpr_recognizer import OpenALPRDetector
 from backend.controllers.plate_recognizer import PlateRecognizerAPI
 from backend.controllers.base_plate_recognizer import BasePlateRecognizer
 
-detector = OpenALPRDetector()
-imagem_teste = "/data/backend/assets/image1.jpg"
+class NovoDetector(BasePlateRecognizer):
+    ...
 
-placas = detector.find_plate(imagem_teste)
-
-print(f'Busca local: {placas}')
-
-detector = PlateRecognizerAPI()
+detector = NovoDetector()
 imagem_teste = "backend/assets/image1.jpg"
 
 placas = detector.find_plate(imagem_teste)
+print(f'Busca local: {placas}')
 
-print(f'Busca API: {placas}')
+
+# detector = OpenALPRDetector()
+# imagem_teste = "/data/backend/assets/image1.jpg"
+
+# placas = detector.find_plate(imagem_teste)
+# print(f'Busca local: {placas}')
+
+# detector = PlateRecognizerAPI()
+# imagem_teste = "backend/assets/image1.jpg"
+
+# placas = detector.find_plate(imagem_teste)
+# print(f'Busca API: {placas}')
