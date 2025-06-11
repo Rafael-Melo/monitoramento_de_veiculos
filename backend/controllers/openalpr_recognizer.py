a16d7b7fa1f5ef01f81e2583a79cf9a77d2575e0
Rafel-Melo <rafael.vilas@gmail.com>
2025-06-11 10:59:38 -0300

    bloco Try na função find_plate

--- backend/controllers/openalpr_recognizer.py
+++ backend/controllers/openalpr_recognizer.py
@@ -7,62 +7,66 @@ from backend.controllers.base_plate_recognizer import BasePlateRecognizer
 class OpenALPRDetector(BasePlateRecognizer):
 
     def find_plate(self, caminho_imagem):
-        comando = [
-            "docker",                       # comando principal do Docker
-            "run",                          # executa o conteúdo
-            "--rm",                         # remove o contêiner após a execução
-            "-v",                           # monta o volume
-            f"{os.getcwd()}:/data:ro",      # mapeia o diretório atual como /data no contêiner
-            "openalpr",                     # nome da imagem Docker a ser usada
-            "-c",                           # definir o país para o reconhecimento
-            "br",                           # código do país: Brasil
-            "-j",                           # saída em formato JSON
-            caminho_imagem
-        ]
+        try:
+            comando = [
+                "docker",                       # comando principal do Docker
+                "run",                          # executa o conteúdo
+                "--rm",                         # remove o contêiner após a execução
+                "-v",                           # monta o volume
+                f"{os.getcwd()}:/data:ro",      # mapeia o diretório atual como /data no contêiner
+                "openalpr",                     # nome da imagem Docker a ser usada
+                "-c",                           # definir o país para o reconhecimento
+                "br",                           # código do país: Brasil
+                "-j",                           # saída em formato JSON
+                caminho_imagem
+            ]
 
-        resultado = subprocess.run(comando, capture_output=True, text=True)
+            resultado = subprocess.run(comando, capture_output=True, text=True)
 
-        print("Diretório atual:", os.getcwd())
+            print("Diretório atual:", os.getcwd())
 
-        if resultado.returncode !=0:
-            print("⚠️ Docker retornou saída vazia.")
-            print(f'Erro na execução do Docker: {resultado.stderr}')
-            print("stderr:", resultado.stderr)
+            if resultado.returncode !=0:
+                print("⚠️ Docker retornou saída vazia.")
+                print(f'Erro na execução do Docker: {resultado.stderr}')
+                print("stderr:", resultado.stderr)
+                return []
+            
+            print("📦 Saída do Docker:")
+            print(resultado.stdout)
+            # print(f"caminho: {os.getcwd()}")
+            # print(resultado)
+
+            dados = json.loads(resultado.stdout)
+            # pprint(dados)
+            placas_detectadas = []
+
+            for resultado in dados.get("results", []):
+                placa_original = resultado.get("plate", "")
+                box = resultado.get("coordinates", [{}])
+
+                xmin = min(p.get('x') for p in box)
+                xmax = max(p.get('x') for p in box)
+                ymin = min(p.get('y') for p in box)
+                ymax = max(p.get('y') for p in box)
+
+                placa_corrigida = self._standardize_plate(placa_original)
+                placa_valida = self._validate_plate(placa_corrigida)
+
+                placas_detectadas.append({
+                    "caminho_imagem": caminho_imagem,
+                    "placa_original": placa_original,
+                    "placa_corrigida": placa_corrigida,
+                    "placa_valida": placa_valida,
+                    "xmin": xmin,
+                    "xmax": xmax,
+                    "ymin": ymin,
+                    "ymax": ymax,
+                })
+            
+            return placas_detectadas
+        except Exception as e:
+            print(f'Erro na detecção da placa: {str(e)}')
             return []
-        
-        print("📦 Saída do Docker:")
-        print(resultado.stdout)
-        # print(f"caminho: {os.getcwd()}")
-        # print(resultado)
-
-        dados = json.loads(resultado.stdout)
-        # pprint(dados)
-        placas_detectadas = []
-
-        for resultado in dados.get("results", []):
-            placa_original = resultado.get("plate", "")
-            box = resultado.get("coordinates", [{}])
-
-            xmin = min(p.get('x') for p in box)
-            xmax = max(p.get('x') for p in box)
-            ymin = min(p.get('y') for p in box)
-            ymax = max(p.get('y') for p in box)
-
-            placa_corrigida = self._standardize_plate(placa_original)
-            placa_valida = self._validate_plate(placa_corrigida)
-
-            placas_detectadas.append({
-                "caminho_imagem": caminho_imagem,
-                "placa_original": placa_original,
-                "placa_corrigida": placa_corrigida,
-                "placa_valida": placa_valida,
-                "xmin": xmin,
-                "xmax": xmax,
-                "ymin": ymin,
-                "ymax": ymax,
-            })
-        
-        return placas_detectadas
 
     def find_plate_in_video(self, caminho_video, frame_interval=30):
         import cv2
